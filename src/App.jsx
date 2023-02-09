d85e5563f1808efd307be814864578d584a9b986
Caroline Kilg <caroline.kilg@capgemini.com>
2023-02-09 11:20:22 +0100

    Dash toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+/** @format */
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 function App() {
   return (
---
+++ src/Dashboard.jsx
@@ -0,0 +1,36 @@
+/** @format */
+
+function App() {
+  onMount(() => {
+    const ctx = document.getElementById("myChart");
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  });
+
+  return (
+    <div>
+      <canvas id="myChart"></canvas>
+    </div>
+  );
+}
+
+export default Dashboard;
